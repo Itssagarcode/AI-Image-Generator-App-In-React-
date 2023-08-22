f896fed258b6b409263aed37a947335417f79983
Itssagarcode <sagarmaithil70@gmail.com>
2023-08-22 22:53:07 +0530

    update

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,11 +7,13 @@ import default_image from '../Assests/default_image.svg';
 function ImageGenerator() {
   const [image_url, setImage_url] = useState('/');
   let inputRef = useRef(null);
+  const [loading,setLoading] =useState(false);
 
   const imageGenerator = async () => {
     if (inputRef.current.value === '') {
       return 0;
     }
+    setLoading(true);
     const response = await fetch(
       "https://api.openai.com/v1/images/generations", 
       {
@@ -19,7 +21,7 @@ function ImageGenerator() {
         headers: {
           "Content-Type": "application/json",
           // Replace the placeholder token with your actual OpenAI API token
-          Authorization: "Bearer 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",
+          Authorization: "Bearer sk-oe8Gbdb0WiQ50VQj5as5T3BlbkFJHOY4uSG2UKCTTzfL1xX",
           "User-Agent": "Chrome"
         },
         body:JSON.stringify({
@@ -32,6 +34,7 @@ function ImageGenerator() {
     let data = await response.json();
     let data_array = data.data;
     setImage_url(data_array[0].url);
+    setLoading(false)
   }
 
   return (
@@ -40,6 +43,11 @@ function ImageGenerator() {
       <div className="img-loading">
         {/* Use double equals (==) for comparison */}
         <div className="image"><img src={image_url === '/' ? default_image : image_url} alt="" /></div>
+   <div className="loading">
+    <div className={loading?"loading-bar-full":"loading-bar"}>
+        <div className={loading?"loading-text":"display-none"}>Loading....</div>
+    </div>
+   </div>
       </div>
       <div className="search-box">
         <input ref={inputRef} type="text" className='search-input' placeholder='Describe what you want to see' />
